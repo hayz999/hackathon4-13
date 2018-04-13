37d1aced6a127357ee2dddafa69056757085ba74
hayz999 <hlzulkoski@gmail.com>
2018-04-13 10:41:07 -0600

    added event handlers to alcohol buttons

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-//access buttons, add click handlers
-const cocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
-const ordinaryUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink'
+//access buttons, add click handler
+// const cocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+// const ordinaryUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink'
+// alcohol ingredient type apis
+const ginUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin'
+const vodkaUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka'
+const tequilaUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Tequila'
+const rumUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Rum'
+
 const cocktailPartyButton = document.querySelector('.beach-party-button')
 const beachPartyButton = document.querySelector('.cocktail-party-button')
+//cocktail party
+const ginButton = document.querySelector('.gin-button')
+const vodkaButton = document.querySelector('.vodka-button')
+//beach party
+const tequilaButton = document.querySelector('.tequila-button')
+const rumButton = document.querySelector('.rum-button')
 
 cocktailPartyButton.addEventListener('click', function () {
-    apiSearch(cocktailUrl)
-    console.log('click')
+    beachPartyButton.classList.add('hidden')
+    cocktailPartyButton.classList.add('hidden')
+    ginButton.classList.remove('hidden')
+    vodkaButton.classList.remove('hidden')
 })
+
 beachPartyButton.addEventListener('click', function () {
-    apiSearch(ordinaryUrl)
-    console.log('click')
+    beachPartyButton.classList.add('hidden')
+    cocktailPartyButton.classList.add('hidden')
+    tequilaButton.classList.remove('hidden')
+    rumButton.classList.remove('hidden')
+})
+
+ginButton.addEventListener('click', function () {
+  apiSearch(ginUrl)
+  ginButton.classList.add('hidden')
+  vodkaButton.classList.add('hidden')
+})
+
+vodkaButton.addEventListener('click', function () {
+  apiSearch(vodkaUrl)
+  ginButton.classList.add('hidden')
+  vodkaButton.classList.add('hidden')
+})
+
+tequilaButton.addEventListener('click', function () {
+  apiSearch(tequilaUrl)
+  tequilaButton.classList.add('hidden')
+  rumButton.classList.add('hidden')
+})
+
+rumButton.addEventListener('click', function () {
+  apiSearch(rumUrl)
+  tequilaButton.classList.add('hidden')
+  rumButton.classList.add('hidden')
 })
 
 function apiSearch (url) {
